94fc9f3df18a83b85bb299d99b4605ca25ae3d09
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-06-26 23:12:06 +0530

    Frontend and Backend was completed successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,74 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Atodo, setAtodo] = useState("")
+  const [Todo, setTodo] = useState([])
+  const [Id, setId] = useState(0)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (Atodo.trim() === "") {
+      alert("Enter a valid Todo");
+    } else {
+      setTodo([...Todo, { todo: Atodo, id: Id, status: false }]);
+      setAtodo("");
+      setId(Id + 1)
+    }
+  }
+
+  const handleDelete = (id) => {
+    const filteredData = Todo.filter((atodo) => id !== atodo.id)
+    setTodo([...filteredData])
+  }
+
+  const handleEdit = (Id) => {
+    const editableTodo = Todo.filter((atodo) => atodo.id === Id)
+    setAtodo(editableTodo[0].todo);
+    handleDelete(Id);
+  }
+
+  const handleStatus = (Id) => {
+    setTodo(
+      Todo.map((atodo) => {
+        if (atodo.id !== Id) {
+          return atodo;
+        } else {
+          return { ...atodo, status: !(atodo.status) }
+        }
+      })
+    )
+  }
+
+  const Template = Todo.map((atodo, index) => {
+    return (
+      <ul key={index} className="atodo" >
+        <li className='todo' style={{ textDecoration: atodo.status ? "line-through" : "none" }}>{atodo.todo}</li>
+        <div className='btns'>
+          <button className='btn delete_btn' onClick={() => handleDelete(atodo.id)}>Delete</button>
+          <button className='btn edit_btn' onClick={() => handleEdit(atodo.id)}>Edit</button>
+          <button className={atodo.status ? "btn completed_btn": "btn incomplete_btn"} onClick={() => handleStatus(atodo.id)}>{atodo.status ? "completed" : "incomplete"}</button>
+        </div>
+      </ul>)
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <form>
+        <input
+          type='text'
+          value={Atodo}
+          onChange={(e) => setAtodo(e.target.value)}
+        />
+        <input
+          type='submit'
+          value={"Add"}
+          onClick={(e) => handleSubmit(e)}
+        />
+      </form>
+
+      <div className='todo_container'>
+        {Template}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
+import './assets/App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <header>
+      <h1 className='logo'>Sigma ~ Todo</h1>
+    </header>
+    <section className='app'>
+      <App />
+    </section>
+    <footer>
+      <h3 className='credits'>Developed and designed by <span className='company'>Sigma Software Solution</span></h3>
+    </footer>
+  </React.StrictMode>
+);
